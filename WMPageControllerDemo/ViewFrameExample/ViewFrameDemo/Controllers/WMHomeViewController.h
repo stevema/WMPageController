d5dc8d4c6725313fd90be038da1a5a5edd84f987
Chunlin Ma <machunlin@diyidan.com>
2017-12-22 11:47:45 +0800

    frame demo support iPhone X

--- WMPageControllerDemo/ViewFrameExample/ViewFrameDemo/Controllers/WMHomeViewController.h
+++ WMPageControllerDemo/ViewFrameExample/ViewFrameDemo/Controllers/WMHomeViewController.h
@@ -8,8 +8,13 @@
 
 #import "WMPageController.h"
 
+#define kScreen_Height   ([UIScreen mainScreen].bounds.size.height)
+#define kScreen_Width    ([UIScreen mainScreen].bounds.size.width)
+// iPhone X
+#define  DD_iPhoneX (kScreen_Width == 375.f && kScreen_Height == 812.f ? YES : NO)
+
 static CGFloat const kWMHeaderViewHeight = 200;
-static CGFloat const kNavigationBarHeight = 64;
+#define kNavigationBarHeight  (DD_iPhoneX?88:64)
 @interface WMHomeViewController : WMPageController
 @property (nonatomic, assign) CGFloat viewTop;
 @end
